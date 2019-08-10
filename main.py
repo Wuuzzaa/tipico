5eaadf019dd536bf653822e8e711d544541cf106
Wuuzzaa <jonas-licht@gmx.de>
2019-08-10 16:16:14 +0200

    main: driver_scrap_test() scraps the "hidden" bet-options

--- main.py
+++ main.py
@@ -2,18 +2,24 @@ from tipico_scraper import TipicoScraper
 from combination_bet_creator import CombinationBetCreator
 from bets_to_excel import BetsToExcel
 
+from bs4 import BeautifulSoup as Bs
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+
 
 def main():
     scraper = TipicoScraper()
     scraper.scrape_all()
     print("########################")
     scraper.sort_matches_by_lowest_quote()
-    scraper.filter_min_lowest_quote(1.15)
-    scraper.filter_max_lowest_quote(1.7)
+    #scraper.filter_min_lowest_quote(1.15)
+    #scraper.filter_max_lowest_quote(1.7)
     scraper.print_matches()
     print("########################")
 
-    cbc = CombinationBetCreator(matches=scraper.matches, combination_size=2)
+    cbc = CombinationBetCreator(matches=scraper.matches, combination_size=3)
     cbc.create_bets_with_outcome_lowest_quote()
     cbc.create_random_combi_bets()
     cbc.refresh_statistics()
@@ -29,6 +35,25 @@ def main():
     saver.save_to_excel()
 
 
+def driver_scrap_test():
+    driver = webdriver.Firefox()
+    driver.get("https://www.tipico.de/de/online-sportwetten/fussball/deutschland/bundesliga/g42301/")
+    soup = Bs(driver.page_source, "html.parser")
+    print(soup.prettify())
+
+    buttons = driver.find_elements_by_xpath("//div[@class='t_more bl align_c right']")
+
+    for button in buttons:
+        button.click()
+
+    pass
+
+    # div class="limits_hover "
+
 if __name__ == "__main__":
-    main()
+    #main()
+    driver_scrap_test()
+
+
+
 
